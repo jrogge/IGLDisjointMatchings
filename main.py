d2c1887a02503477d5986fc6e897e80e5664494e
jrogge <jwilrogge@gmail.com>
2019-03-09 10:23:24 -0600

    remove elements

--- main.py
+++ main.py
@@ -10,22 +10,19 @@ MATCHING_COLOR = 'yellow'
 ADD_NODE = 0
 ADD_EDGE = 1
 ADD_PATH = 2
+REMOVE_NODE = 3
+REMOVE_EDGE = 4
 mode = ADD_NODE
 graph = nx.Graph()
 node_list = []
 new_edge_first = -1
 filename = ""
 
-def draw_graph():
+def setup_graph():
     for node in graph.nodes(data=True):
-        print("node:", node)
         coords = node[-1]['coord']
-        canvas.create_oval(coords[0]-radius,coords[1]-radius,
-                coords[0]+radius,coords[1]+radius)
+        add_node(coords[0], coords[1])
     for edge in graph.edges():
-        #print("edge:", edge)
-        #print("graph[edge[0]]", graph[edge[0]])
-        #print("graph.nodes[edge[0]]", graph.nodes[edge[0]])
         x0 = graph.nodes[edge[0]]['coord'][0]
         y0 = graph.nodes[edge[0]]['coord'][1]
         x1 = graph.nodes[edge[1]]['coord'][0]
@@ -40,19 +37,29 @@ def add_node(x, y):
     y0 = y - radius
     x1 = x + radius
     y1 = y + radius
-    canvas.create_oval(x0,y0,x1,y1)
+    new_obj = canvas.create_oval(x0,y0,x1,y1)
     global indx
-    graph.add_node(indx, coord=[x,y])
+    graph.add_node(indx, coord=[x,y], obj=new_obj)
     indx += 1
+
+def remove_node(x, y):
+    node = get_nearest_node(x,y)
+    # delete adjacent edges
+    adj_list = graph[node]
+    for adj_node in adj_list:
+        remove_edge(node, adj_node)
+    graph.remove_node(node)
+    canvas.delete(graph.nodes[node]['obj'])
 
 def get_nearest_node(x, y):
     dist = -1
     index = -1
-    for ind, node in enumerate(node_list):
-        curr_dist = (node[0] - x)**2 + (node[1] - y)**2
+    for node in graph.nodes(data=True):
+        curr_coords = node[-1]['coord']
+        curr_dist = (curr_coords[0] - x)**2 + (curr_coords[1] - y)**2
         if (curr_dist < dist) or (dist == -1):
             dist = curr_dist
-            index = ind
+            index = node[0]
 
     return index
 
@@ -94,6 +101,31 @@ def key(event):
         print("path mode")
     elif char == 's':
         gutils.save_graph(graph, filename)
+    elif char == 'g':
+        mode = REMOVE_EDGE
+        new_edge_first = -1
+        print("remove edge mode")
+    elif char == 'r':
+        mode = REMOVE_NODE
+        new_edge_first = -1
+
+def remove_edge(u, v):
+    old_obj = graph[u][v]['obj']
+    canvas.delete(old_obj)
+    graph.remove_edge(u, v)
+
+def remove_edge_click(x,y):
+    global new_edge_first
+    nearest_index = get_nearest_node(x, y)
+    if new_edge_first == -1:
+        new_edge_first = nearest_index
+    else:
+        remove_edge(new_edge_first, nearest_index)
+
+        if mode == REMOVE_EDGE:
+            # reset this
+            new_edge_first = -1
+
 
 def add_edge_click(x,y):
     global new_edge_first
@@ -101,11 +133,9 @@ def add_edge_click(x,y):
     if new_edge_first == -1:
         new_edge_first = nearest_index
     else:
-        x0 = node_list[new_edge_first][0]
-        y0 = node_list[new_edge_first][1]
-        x1 = node_list[nearest_index][0]
-        y1 = node_list[nearest_index][1]
-        new_edge_obj = canvas.create_line(x0,y0,x1,y1)
+        v0 = graph.nodes[nearest_index]['coord']
+        v1 = graph.nodes[new_edge_first]['coord']
+        new_edge_obj = canvas.create_line(v0[0],v0[1],v1[0],v1[1])
         graph.add_edge(new_edge_first, nearest_index, obj = new_edge_obj)
 
         if mode == ADD_EDGE:
@@ -117,22 +147,26 @@ def add_edge_click(x,y):
 
 def click(event):
     if mode == ADD_NODE:
-        #add_node(indx, coord=[event.x, event.y])
         add_node(event.x, event.y)
+    elif mode == REMOVE_NODE:
+        remove_node(event.x, event.y)
     elif (mode == ADD_EDGE) or (mode == ADD_PATH):
         add_edge_click(event.x, event.y)
+    elif (mode == REMOVE_EDGE):
+        remove_edge_click(event.x, event.y)
 
 def motion(event):
     #print("motion!")
-    if len(node_list) < 1:
+    if graph.number_of_nodes() < 1:
         return
 
     global indicator # is this bad practice? Probably should wrap this up into a class
     if indicator != -1:
         canvas.delete(indicator)
     index = get_nearest_node(event.x, event.y)
-    node = node_list[index]
-    indicator = canvas.create_line(event.x, event.y, node[0], node[1], fill='green')
+    coords = graph.nodes[index]['coord']
+    indicator = canvas.create_line(event.x, event.y, coords[0], coords[1],
+            fill='green')
 
 
 # Tkinter boilerplate
@@ -154,7 +188,7 @@ canvas.bind("<Motion>", motion)
 canvas.bind("<Key>", key)
 ######## load graph manually ########
 graph = gutils.load_graph("counter.txt")
-draw_graph()
+setup_graph()
 #load()
 #canvas.bind("<Key>", redrawDirectionMarker)
 #canvas.bind("<B1-Motion>", drag)
